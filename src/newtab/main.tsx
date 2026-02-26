fcb10a4a4a79f2c51a9ea6fa0bfbd243bafa9cb4
Xavier Brinon <xavier@brinon.eu>
2026-02-26 16:34:57 +0000

    feat: wire QueryClientProvider into React entry point
    main.tsx now wraps App in QueryClientProvider using createQueryClient()
    from storage module. All components can use TanStack Query hooks.

--- src/newtab/main.tsx
+++ src/newtab/main.tsx
@@ -1,5 +1,13 @@
+import { QueryClientProvider } from "@tanstack/react-query";
 import { createRoot } from "react-dom/client";
+import { createQueryClient } from "../storage";
 import { App } from "./App";
 import "./App.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const queryClient = createQueryClient();
+
+createRoot(document.getElementById("root")!).render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>,
+);
